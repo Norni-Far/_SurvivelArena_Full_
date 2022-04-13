b6ab14150ef03bf8b5f26c5b9b731f509742eded
Norni-Far <the.somnium.project.games@gmail.com>
2022-04-13 19:36:46 +0300

    dima_Add

--- Assets/Scripts/Hero/S_bulletMoveTowardsTarget.cs
+++ Assets/Scripts/Hero/S_bulletMoveTowardsTarget.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class S_bulletMoveTowardsTarget : MonoBehaviour
 {
-
     public float speed = 0.1f;
-
-
 
     void FixedUpdate()
     {
-
         transform.position += transform.up * speed;
-
     }
 }
